a2792596387c7647652474c0fca135ef643c8e53
minyoung62 <tnd05106@naver.com>
2022-06-22 20:50:00 +0900

    feat: 오운완 프로젝트 셋팅

---
+++ src/main/java/com/owoonan/owoonan/OwoonanApplication.java
@@ -0,0 +1,13 @@
+package com.owoonan.owoonan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwoonanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwoonanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/owoonan/owoonan/OwoonanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owoonan.owoonan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwoonanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
