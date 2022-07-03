e4773bddce89fe1d7e06f69c3943593ea636e674
김민영 <unknown>
2022-07-03 15:04:33 +0900

    feat: api 명세서를 위한 swagger init 및 테스트

---
+++ src/main/java/com/owoonan/owoonan/global/config/swagger/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.owoonan.owoonan.global.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket restAPI() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.owoonan"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(" 오운완 REST API")
+                .version("1.0.0")
+                .description("오운완 REST API 명세서")
+                .build();
+    }
+}
