690c492343443f713eea6c3be06de25054fe9c02
김민영 <>
2022-07-06 15:48:53 +0900

    feat: swagger jwt 토큰 설정
    header에 jwt 넣어주는 기능 추가

--- src/main/java/com/owoonan/owoonan/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/owoonan/owoonan/global/config/swagger/SwaggerConfig.java
@@ -6,29 +6,64 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
+    private static final String apiTitle = "오운완 REST API";
+    private static final String apiVersion = "1.0.0";
+    private static final String apiDescription = "오운완 REST API 명세서";
+
     @Bean
     public Docket restAPI() {
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.owoonan"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()));
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title(" 오운완 REST API")
-                .version("1.0.0")
-                .description("오운완 REST API 명세서")
+                .title(apiTitle)
+                .version(apiVersion)
+                .description(apiDescription)
                 .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return springfox
+                .documentation
+                .spi.service
+                .contexts
+                .SecurityContext
+                .builder()
+                .securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
     }
 }
