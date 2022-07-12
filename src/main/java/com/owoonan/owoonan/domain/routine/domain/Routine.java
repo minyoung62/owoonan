1dc907649b6488ed7570496e814082684bcf1c77
김민영 <>
2022-07-12 10:28:05 +0900

    feat: 인절을 이용한 여러개 운동조회 추가

--- src/main/java/com/owoonan/owoonan/domain/routine/domain/Routine.java
+++ src/main/java/com/owoonan/owoonan/domain/routine/domain/Routine.java
@@ -1,12 +1,11 @@
 package com.owoonan.owoonan.domain.routine.domain;
 
+import com.owoonan.owoonan.domain.routine.domain.vo.Part;
 import com.owoonan.owoonan.domain.user.domain.User;
+import com.owoonan.owoonan.domain.workout.domain.Workout;
 import com.owoonan.owoonan.domain.workoutroutine.domain.WorkoutRoutine;
 import com.owoonan.owoonan.global.common.BaseEntity;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -16,6 +15,8 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
+@Table(name = "routine")
 public class Routine extends BaseEntity {
 
     @Id
@@ -26,11 +27,14 @@ public class Routine extends BaseEntity {
     @Column(length = 40,nullable = false)
     private String routineName;
 
+    @OneToMany(mappedBy = "routine", cascade = CascadeType.ALL)
+    private List<Part> parts = new ArrayList<>();
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToMany(mappedBy = "routine",cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "routine",  cascade = CascadeType.ALL)
     private List<WorkoutRoutine> workoutRoutines = new ArrayList<>();
 
     @Builder
@@ -38,5 +42,30 @@ public class Routine extends BaseEntity {
         this.routineId = routineId;
         this.routineName = routineName;
         this.user = user;
+    }
+
+    public void addUser(User user) {
+        this.user = user;
+    }
+
+    public void addWorkoutRoutines(List<WorkoutRoutine> workoutRoutines) {
+        for(WorkoutRoutine workoutRoutine: workoutRoutines) {
+            this.workoutRoutines.add(workoutRoutine);
+            workoutRoutine.addRoutine(this);
+        }
+    }
+
+    public void addParts(List<Part> parts) {
+        for(Part part: parts) {
+            System.out.println("tt"+part);
+            this.parts.add(part);
+            part.addRoutine(this);
+        }
+    }
+
+    public void patch(Routine updateRoutine, List<WorkoutRoutine> workoutRoutines, List<Part> parts) {
+        this.routineName = updateRoutine.getRoutineName();
+        addWorkoutRoutines(workoutRoutines);
+        addParts(parts);
     }
 }
