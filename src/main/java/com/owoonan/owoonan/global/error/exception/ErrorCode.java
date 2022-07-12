320a142f4b9019e776e183c654cf613d80f8967c
김민영 <unknown>
2022-07-12 10:20:33 +0900

    feat: error 코드 추가

--- src/main/java/com/owoonan/owoonan/global/error/exception/ErrorCode.java
+++ src/main/java/com/owoonan/owoonan/global/error/exception/ErrorCode.java
@@ -12,12 +12,16 @@ public enum ErrorCode {
 
     // User
     USER_NOT_FOUND(500, "U001", "해당 유저가 존재하지 않습니다"),
+    USER_ALREADY_REGISTER(500, "U002", "이미 가입한 유저입니다"),
 
     // Workout
     WORKOUT_NOT_FOUND(501, "W001", "해당 운동이 존재하지 않습니다"),
     WORKOUT_MISS_MATCH(502, "W001", "해당 사용자의 운동이 아닙니다"),
-    WORKOUT_NAME_DUPLICATION(503, "W003", "중복된 운동 이름 입니다");
+    WORKOUT_NAME_DUPLICATION(503, "W003", "중복된 운동 이름 입니다"),
 
+    // Routine
+    ROUTINE_NOT_FOUND(504, "R001", "해당 운동 루틴은 존재하지 않습니다"),
+    ROUTINE_MISS_MATCH(505, "R002", "해당 사용자의 루틴이 아닙니다");
     private final int status;
     private final String code;
     private final String message;
