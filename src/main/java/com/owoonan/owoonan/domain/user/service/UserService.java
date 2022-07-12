e1b7e4c3fd3e0c2c583312801219ea7dc9b09b0b
김민영 <unknown>
2022-07-12 10:18:33 +0900

    feat:  사용자 등록 기능 추가
    사용자 등록 시 기본운동 셋팅

--- src/main/java/com/owoonan/owoonan/domain/user/service/UserService.java
+++ src/main/java/com/owoonan/owoonan/domain/user/service/UserService.java
@@ -1,13 +1,19 @@
 package com.owoonan.owoonan.domain.user.service;
 
 import com.owoonan.owoonan.domain.user.domain.User;
+import com.owoonan.owoonan.domain.user.domain.vo.DefaultWorkout;
+import com.owoonan.owoonan.domain.user.domain.vo.RoleType;
 import com.owoonan.owoonan.domain.user.dto.UserResponseDto;
+import com.owoonan.owoonan.domain.user.error.UserAlreadyRegisterException;
 import com.owoonan.owoonan.domain.user.error.UserNotFoundException;
+import com.owoonan.owoonan.domain.workout.domain.Workout;
 import com.owoonan.owoonan.global.error.exception.ErrorCode;
 import com.owoonan.owoonan.global.jwt.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Service
@@ -17,17 +23,27 @@ public class UserService {
     private final UserRepository userRepository;
 
     @Transactional(readOnly = true)
-
-    public UserResponseDto getUser(final String userId) {
-        User user = userRepository.findByUserId(userId);
-        if (user == null) throw new UserNotFoundException(ErrorCode.USER_NOT_FOUND);
+    public UserResponseDto findUser(final String userId) {
+        User user = getUser(userId);
         return UserResponseDto.of(user);
     }
 
     public void deleteUser(final String userId) {
-        User user = userRepository.findByUserId(userId);
-        if (user == null) throw new UserNotFoundException(ErrorCode.USER_NOT_FOUND);
+        User user = getUser(userId);
         userRepository.delete(user);
     }
 
+    public void registerUser(final String userId) {
+        User user = getUser(userId);
+        if(user.getRoleType() != RoleType.GUEST) throw new UserAlreadyRegisterException(ErrorCode.USER_ALREADY_REGISTER);
+        List<Workout> workouts = DefaultWorkout.init(user);
+        user.setWorkouts(workouts);
+        user.setRoleType(RoleType.USER);
+        userRepository.save(user);
+    }
+    private User getUser(String userId) {
+        User user = userRepository.findByUserId(userId);
+        if (user == null) throw new UserNotFoundException(ErrorCode.USER_NOT_FOUND);
+        return user;
+    }
 }
