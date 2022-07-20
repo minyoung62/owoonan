a5a85894989303900344326bc20aabdfa4bdede1
김민영 <>
2022-07-20 19:37:39 +0900

    feat: 쿼리 추가
    파이차트, 막대그래프에 필요한 데이터 요청 쿼리

--- src/main/java/com/owoonan/owoonan/domain/record/repository/RecordRepositoryCustom.java
+++ src/main/java/com/owoonan/owoonan/domain/record/repository/RecordRepositoryCustom.java
@@ -3,6 +3,9 @@ package com.owoonan.owoonan.domain.record.repository;
 import com.owoonan.owoonan.domain.record.dto.DayRecord;
 import com.owoonan.owoonan.domain.record.dto.ResultWorkoutRecordDto;
 import com.owoonan.owoonan.domain.record.dto.WorkoutRecordDto;
+import com.owoonan.owoonan.domain.record.dto.chart.LineChartSearch;
+import com.owoonan.owoonan.domain.record.dto.chart.ResultLineChart;
+import com.owoonan.owoonan.domain.record.dto.chart.ResultPieChart;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -11,4 +14,8 @@ public interface RecordRepositoryCustom {
     List<ResultWorkoutRecordDto>  findRecordForTheMonth(LocalDate localDate, String userId);
 
     List<DayRecord> findRecordsByTheMonth(LocalDate localDate, String userId);
+
+    ResultPieChart getPieChart(String userId);
+
+    ResultLineChart getLineChart(LineChartSearch lineChartSearch, String userId);
 }
--- src/main/java/com/owoonan/owoonan/domain/record/repository/RecordRepositoryImpl.java
+++ src/main/java/com/owoonan/owoonan/domain/record/repository/RecordRepositoryImpl.java
@@ -1,8 +1,12 @@
 package com.owoonan.owoonan.domain.record.repository;
 
 import com.owoonan.owoonan.domain.record.dto.*;
+import com.owoonan.owoonan.domain.record.dto.chart.*;
+import com.owoonan.owoonan.domain.workout.domain.QWorkout;
 import com.owoonan.owoonan.domain.workout.domain.vo.WorkoutPart;
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,13 +16,12 @@ import lombok.RequiredArgsConstructor;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static com.owoonan.owoonan.domain.record.domain.QRecord.record;
 import static com.owoonan.owoonan.domain.workout.domain.QWorkout.workout;
+import static com.owoonan.owoonan.domain.workout.domain.vo.WorkoutPart.*;
 
 @RequiredArgsConstructor
 public class RecordRepositoryImpl implements RecordRepositoryCustom{
@@ -84,7 +87,8 @@ public class RecordRepositoryImpl implements RecordRepositoryCustom{
         List<LocalDate> resultLocalDates = result.stream().map(r -> r.getLocalDate())
                 .collect(Collectors.toList());
 
-        List<WorkoutRecordDto> workoutRecordDtos = query.select(Projections.constructor(WorkoutRecordDto.class, record.saveTime, workout.workoutId, workout.workoutName, workout.workoutPart))
+        List<WorkoutRecordDto> workoutRecordDtos = query.select(Projections.constructor(WorkoutRecordDto.class,
+                        record.saveTime, workout.workoutId, workout.workoutName, workout.workoutPart))
                 .from(workout).distinct()
                 .join(workout.records, record)
                 .where(record.userId.eq(userId), record.saveTime.in(resultLocalDates))
@@ -95,26 +99,31 @@ public class RecordRepositoryImpl implements RecordRepositoryCustom{
                 .collect(Collectors.toList());
 
 
-        List<RecordResponseDto> recordResponseDtos = query.select(Projections.constructor(RecordResponseDto.class, record.saveTime, record.workout.workoutId, record.recordId, record.weight, record.rep))
+        List<RecordResponseDto> recordResponseDtos = query.select(Projections.constructor(RecordResponseDto.class,
+                        record.saveTime, record.workout.workoutId, record.recordId, record.weight, record.rep))
                 .from(record)
                 .where(record.saveTime.in(resultLocalDates), record.workout.workoutId.in(workoutIds))
                 .fetch();
 
         Map<LocalDate, Map<Long, List<RecordResponseDto>>> collect = recordResponseDtos.stream()
-                .collect(Collectors.groupingBy(RecordResponseDto::getLocalDate, Collectors.groupingBy(RecordResponseDto::getWorkoutId)));
+                .collect(Collectors.groupingBy(RecordResponseDto::getLocalDate,
+                        Collectors.groupingBy(RecordResponseDto::getWorkoutId)));
 
         workoutRecordDtos.forEach(w -> w.setRecordResponseDtos(collect.get(w.getLocalDate()).get(w.getWorkoutId())));
 
-        List<ResultWorkoutRecordDto> resultWorkoutRecordDtos = query.select(Projections.constructor(ResultWorkoutRecordDto.class, record.saveTime, workout.workoutPart))
+        List<ResultWorkoutRecordDto> resultWorkoutRecordDtos = query.select(
+                Projections.constructor(ResultWorkoutRecordDto.class, record.saveTime, workout.workoutPart))
                 .from(record).distinct()
                 .join(record.workout, workout)
                 .where(record.userId.eq(userId), record.saveTime.between(start, end))
                 .fetch();
 
         Map<LocalDate, Map<WorkoutPart, List<WorkoutRecordDto>>> workoutRecordDtoMap = workoutRecordDtos.stream()
-                .collect(Collectors.groupingBy(WorkoutRecordDto::getLocalDate, Collectors.groupingBy(WorkoutRecordDto::getWorkoutPart)));
+                .collect(Collectors.groupingBy(WorkoutRecordDto::getLocalDate,
+                        Collectors.groupingBy(WorkoutRecordDto::getWorkoutPart)));
 
-        resultWorkoutRecordDtos.forEach(r -> r.setWorkoutRecordDtos(workoutRecordDtoMap.get(r.getLocalDate()).get(r.getWorkoutPart())));
+        resultWorkoutRecordDtos.
+                forEach(r -> r.setWorkoutRecordDtos(workoutRecordDtoMap.get(r.getLocalDate()).get(r.getWorkoutPart())));
 
 
         Map<LocalDate, List<ResultWorkoutRecordDto>> resultWorkoutRecordDtoMap = resultWorkoutRecordDtos.stream()
@@ -125,5 +134,79 @@ public class RecordRepositoryImpl implements RecordRepositoryCustom{
         return result;
     }
 
+    @Override
+    public ResultPieChart getPieChart(String userId) {
+        ArrayList<WorkoutPart> workoutParts = new ArrayList<>(Arrays.asList(CHEST, SHOULDER, BACK, ARM, LEG, ABS));
 
+        List<PieChartOfWorkout> pieCharOfWorkouts = query.select(Projections.constructor(PieChartOfWorkout.class,
+                        workout.workoutName,
+                        workout.workoutPart,
+                        record.weight.multiply(record.rep).sum()))
+                .from(workout)
+                .join(workout.records, record)
+                .where(workout.user.userId.eq(userId))
+                .groupBy(workout)
+                .fetch();
+
+        Map<WorkoutPart, List<PieChartOfWorkout>> workoutPartsMap = pieCharOfWorkouts.stream()
+                .collect(Collectors.groupingBy(PieChartOfWorkout::getWorkoutPart));
+
+        HashMap<WorkoutPart, Double> hashMap = new HashMap<>();
+        workoutPartsMap.keySet().forEach(w -> {
+            Double sum = workoutPartsMap.get(w).stream().mapToDouble(i -> i.getWeightOfWorkout()).sum();
+            hashMap.put(w, sum);
+        });
+
+        double total = hashMap.keySet().stream()
+                .mapToDouble(k -> hashMap.get(k)).sum();
+
+        pieCharOfWorkouts.forEach(p -> {
+            p.setRatioOfWorkout((p.getWeightOfWorkout()/hashMap.get(p.getWorkoutPart())*100.0));
+        });
+
+        List<PieChartOfWorkoutPart> pieChartOfWorkoutPart = workoutParts.stream()
+                .map(w -> new PieChartOfWorkoutPart(w, 0.0, 0.0))
+                .collect(Collectors.toList());
+
+
+        Map<WorkoutPart, PieChartOfWorkoutPart> pieChartOfWorkoutPartMap = new HashMap<>();
+
+        pieChartOfWorkoutPart.forEach(p -> pieChartOfWorkoutPartMap.put(p.getWorkoutPart(), p));
+
+        workoutPartsMap.keySet().forEach(p -> {
+            pieChartOfWorkoutPartMap.get(p)
+                    .setPieChartOfWorkouts(workoutPartsMap.get(p));
+            pieChartOfWorkoutPartMap.get(p)
+                    .setWeightOfWorkoutPart(hashMap.get(p));
+            pieChartOfWorkoutPartMap.get(p)
+                    .setRatioOfWorkoutPart((double) Math.round((hashMap.get(p)/total)*100.0));
+        });
+
+
+        return new ResultPieChart(pieChartOfWorkoutPart);
+    }
+
+    public ResultLineChart getLineChart(LineChartSearch lineChartSearch, String userId) {
+        List<Volume> result = query.select(Projections.constructor(Volume.class, record.saveTime, record.weight.multiply(record.rep).sum()))
+                .from(record)
+                .join(record.workout, workout)
+                .where(record.userId.eq(userId),
+                        WorkoutPartEq(lineChartSearch.getWorkoutPart()),
+                        WorkoutEq(lineChartSearch.getWorkout()))
+                .groupBy(record.saveTime)
+                .orderBy(record.saveTime.desc())
+                .offset(lineChartSearch.getOffset())
+                .limit(lineChartSearch.getLimit())
+                .fetch();
+
+        return new ResultLineChart(result);
+    }
+
+    private BooleanExpression WorkoutEq(String workout) {
+        return workout != null ? QWorkout.workout.workoutName.eq(workout) : null;
+    }
+
+    private com.querydsl.core.types.dsl.BooleanExpression WorkoutPartEq(WorkoutPart workoutPart) {
+        return workoutPart != null ? workout.workoutPart.eq(workoutPart) : null;
+    }
 }
