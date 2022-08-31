2fc2f4c8784f8d262916b87f36209832d59705e2
김민영 <tnd05106@naver.com>
2022-08-31 17:44:26 +0900

    feat:  포스트 생성 서비스

--- src/main/java/com/owoonan/owoonan/domain/post/repository/PostRepository.java
+++ src/main/java/com/owoonan/owoonan/domain/post/repository/PostRepository.java
@@ -1,7 +1,10 @@
 package com.owoonan.owoonan.domain.post.repository;
 
 import com.owoonan.owoonan.domain.post.domain.Post;
+import com.owoonan.owoonan.domain.post.dto.PostResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 }
