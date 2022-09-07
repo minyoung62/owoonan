d5ceffbc392aa256288e660e12b554d2550b5d97
김민영 <tnd05106@naver.com>
2022-09-08 00:17:35 +0900

    feat: post요청시 사용자 필드추가

--- src/main/java/com/owoonan/owoonan/domain/post/dto/PostResponseDto.java
+++ src/main/java/com/owoonan/owoonan/domain/post/dto/PostResponseDto.java
@@ -11,14 +11,16 @@ import java.util.List;
 public class PostResponseDto {
     private Long postId;
     private String contents;
+    private String username;
     private List<ImageDto> imageDtos;
     private Long commentCount;
     private Long likelyCount;
     private boolean isLike = false;
 
-    public PostResponseDto(Long postId, String contents, Long commentCount, Long likelyCount) {
+    public PostResponseDto(Long postId, String contents ,String username ,Long commentCount, Long likelyCount) {
         this.postId = postId;
         this.contents = contents;
+        this.username = username;
         this.commentCount = commentCount;
         this.likelyCount = likelyCount;
     }
