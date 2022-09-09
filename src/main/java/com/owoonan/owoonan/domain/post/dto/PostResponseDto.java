70ced0aceb3bdf7dba168cf2ae125d5ec9b5b759
김민영 <tnd05106@naver.com>
2022-09-09 18:05:49 +0900

    feat: userId 및 생성, 수정 시간 추가

--- src/main/java/com/owoonan/owoonan/domain/post/dto/PostResponseDto.java
+++ src/main/java/com/owoonan/owoonan/domain/post/dto/PostResponseDto.java
@@ -4,24 +4,38 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Setter
 public class PostResponseDto {
-    private Long postId;
-    private String contents;
-    private String username;
-    private List<ImageDto> imageDtos;
-    private Long commentCount;
-    private Long likelyCount;
-    private boolean isLike = false;
+  private Long postId;
+  private String contents;
+  private String username;
+  private String userId;
+  private List<ImageDto> imageDtos;
+  private Long commentCount;
+  private LocalDateTime createdTime;
+  private LocalDateTime lastModifiedTime;
+  private Long likelyCount;
+  private boolean isLike = false;
 
-    public PostResponseDto(Long postId, String contents ,String username ,Long commentCount, Long likelyCount) {
-        this.postId = postId;
-        this.contents = contents;
-        this.username = username;
-        this.commentCount = commentCount;
-        this.likelyCount = likelyCount;
-    }
+  public PostResponseDto(Long postId,
+                         String contents,
+                         String username,
+                         String userId,
+                         Long commentCount,
+                         Long likelyCount,
+                         LocalDateTime createdTime,
+                         LocalDateTime lastModifiedTime) {
+    this.postId = postId;
+    this.contents = contents;
+    this.username = username;
+    this.userId = userId;
+    this.commentCount = commentCount;
+    this.likelyCount = likelyCount;
+    this.createdTime = createdTime;
+    this.lastModifiedTime = lastModifiedTime;
+  }
 }
