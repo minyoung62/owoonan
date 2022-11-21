807791b76bb71170622c8389fd7432b958725b36
김민영 <tnd05106@naver.com>
2022-11-21 23:32:30 +0900

    feat: 운동기록 엔티티 삭제

--- src/main/java/com/owoonan/owoonan/domain/routine/repository/RoutineRepository.java
+++
@@ -1,7 +0,0 @@
-package com.owoonan.owoonan.domain.routine.repository;
-
-import com.owoonan.owoonan.domain.routine.domain.Routine;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoutineRepository extends JpaRepository<Routine, Long>, RoutineRepositoryCustom {
-}
--- src/main/java/com/owoonan/owoonan/domain/workout/repository/WorkoutRepository.java
+++
@@ -1,7 +0,0 @@
-package com.owoonan.owoonan.domain.workout.repository;
-
-import com.owoonan.owoonan.domain.workout.domain.Workout;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkoutRepository extends JpaRepository<Workout, Long>, WorkoutRepositoryCustom {
-}
