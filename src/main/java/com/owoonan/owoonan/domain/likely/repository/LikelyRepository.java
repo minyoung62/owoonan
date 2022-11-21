80a079346af619b2a989ea876152760732cf5d92
김민영 <tnd05106@naver.com>
2022-11-21 23:32:30 +0900

    feat: 운동기록 엔티티 삭제

--- src/main/java/com/owoonan/owoonan/domain/likely/repository/LikelyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.owoonan.owoonan.domain.likely.repository;
-
-import com.owoonan.owoonan.domain.likely.domain.Likely;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LikelyRepository extends JpaRepository<Likely, Long> {
-}
