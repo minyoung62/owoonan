64bb9a6301c839fa2385c98aef779e8d845b71ea
김민영 <tnd05106@naver.com>
2022-11-21 23:53:04 +0900

    feat: like entity추가

---
+++ src/main/java/com/owoonan/owoonan/domain/likely/repository/LikelyRepository.java
@@ -0,0 +1,7 @@
+package com.owoonan.owoonan.domain.likely.repository;
+
+import com.owoonan.owoonan.domain.likely.domain.Likely;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikelyRepository extends JpaRepository<Likely, Long> {
+}
